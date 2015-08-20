19580821be3ea01efa3349d79492bff89f3ac46d
Elias Ojala <elias.jh.ojala@gmail.com>
2015-08-20 11:46:49 +0300

    Made receiveDmx() a bit cleaner

--- fog_software/DMX.ino
+++ fog_software/DMX.ino
@@ -1,17 +1,7 @@
-int valueFromDMX = 0;
 int receiveDMX(int ch) {
-  // Determine time elapsed since last received DMX packet
-  unsigned long lastPacket = DMXSerial.noDataSince();
-  
-  if (lastPacket < 800) {
-    // read recent DMX values and set pwm levels
-    valueFromDMX = DMXSerial.read(ch);
-    return valueFromDMX;
-  }
-  else {
-    // After a certain amount of time without any received DMX signal, force the fog off
-    return 0;
-  }
+  //Return value from specific channel or zero if it hasn't been updated in last 800ms
+  if (DMXSerial.noDataSince() < 800) return DMXSerial.read(ch);
+  else return 0;
 }
 
 // Returns true if DMX value received is over 50%.
