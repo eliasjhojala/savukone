a298f92b7fcbca4179d7d5f780af1576d9996556
Elias Ojala <elias.jh.ojala@gmail.com>
2015-08-20 11:48:01 +0300

    cleaned void loop

--- fog_software/main_functions.ino
+++ fog_software/main_functions.ino
@@ -5,12 +5,6 @@ void loop() {
   readTemp(); //Read temperature
   controlHeating(); //Turn resistor on/off according to temperature
 
-  // fogPlease is set to true if any input demands fog
-  boolean fogPlease = false;
-  if(fogButtonPressed()) { fogPlease = true; }
-  if(fogFromDMX()) { fogPlease = true; }
-  
-  // If fogPlease is on, call fogNow()
-  if(fogPlease) { fogNowWithoutAnyCheck(); }
+  if(fogButtonPressed() || fogFromDMX()) { fogNow(); }
   else { stopFog(); }
 }
