6a58df5b3ca0b3818421cfc96d0e527238846c7a
Elias Ojala <elias.jh.ojala@gmail.com>
2015-11-27 19:42:24 +0200

    temperature update

--- fog_software/fog_software.ino
+++ fog_software/fog_software.ino
@@ -2,9 +2,9 @@
 
 int DMXChannel = 1; //Variable to store DMXchannel (this will change from dipswitch)
 
-int lim_lo = 180; //Lowest temperature where we can fog
-int lim_hi = 230; //Highest temperature which resistor is allowed to be
-int lim_dangerous = 260;
+int lim_lo = 190; //Lowest temperature where we can fog
+int lim_hi = 240; //Highest temperature which resistor is allowed to be
+int lim_dangerous = 270;
 
 int rawValue; //Measured value from thermocouple amplifier
 int temperature; //Measured temperature
@@ -20,3 +20,4 @@ const int fogButtonPin = A4; //Where the manual fog button is plugged
 
 const int dipSwitchPinStart = 4; //First dipSwitch pin
 const int dipSwitchPinAmount = 10; //Number of "bits" in dipSwitch
+
